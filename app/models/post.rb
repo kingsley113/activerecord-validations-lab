ccc1c775fb40be07fb07f3042c54e88d3c6c5c59
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-07-16 23:20:42 -0700

    commit tree 2a95408a58eec5b447d229bca0bd80013eb4b4aa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum: 250}
+	validates :summary, length: {maximum: 250}
+	validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+	validate :title_clickbait
+	# validates :title, inclusion: {in: %w(Won't Believe Secret Top [number] Guess)}
+
+	def title_clickbait
+		clickbait = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+		terms = []
+		clickbait.each do |term|
+			if title && title.include?(term)
+				terms << title
+			end
+		end
+		
+		if terms.length == 0
+			errors.add(:title, "must contain clickbait words")
+		end		
+	end
+
 end
